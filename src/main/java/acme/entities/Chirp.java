251c5d93726566b75c2f10bb9f1a0242090ed6aa
Jose Luis Garcia Marin <josgarmar31@alum.us.es>
2022-03-08 11:22:48 +0100

    Tarea 028: hecha

---
+++ src/main/java/acme/entities/Chirp.java
@@ -0,0 +1,45 @@
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Chirp extends AbstractEntity{
+	
+	protected static final long serialVersionUID = 1L;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date creationMoment;
+	
+	@NotBlank
+	@Length(min = 1, max = 101)
+	protected String title;
+	
+	@NotBlank
+	@Length(min = 1, max = 101)
+	protected String author;
+	
+	@NotBlank
+	@Length(min = 1, max = 256)
+	protected String body;
+	
+	@Email
+	protected String email;
+	
+
+}
