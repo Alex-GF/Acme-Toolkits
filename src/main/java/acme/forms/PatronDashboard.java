9344931fdebbe1d3f6033cbbd3ee420a8f6a5d99
Alex-GF <yalejandro9@gmail.com>
2022-03-14 12:09:34 +0100

    Tarea 035: hecha
    Requiere revisión en conjunto por parte del grupo antes de subir a
    master.

---
+++ src/main/java/acme/forms/PatronDashboard.java
@@ -0,0 +1,32 @@
+package acme.forms;
+
+import java.io.Serializable;
+
+public class PatronDashboard implements Serializable{
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	Integer						numberOfProposedPatronages;
+	Integer						numberOfAcceptedPatronages;
+	Integer						numberOfDeniedPatronages;
+	Double						averageNumberOfBudgetsProposedByCurrency;
+	Double						averageNumberOfBudgetsAccetedByCurrency;
+	Double						averageNumberOfBudgetsDeniedByCurrency;
+	Double						deviationOfBudgetsProposedByCurrency;
+	Double						deviationOfBudgetsAcceptedByCurrency;
+	Double						deviationOfBudgetsDeniedByCurrency;
+	Double						minBudgetProposedByCurrency;
+	Double						minBudgetAcceptedByCurrency;
+	Double						minBudgetDeniedByCurrency;
+	Double						maxBudgetProposedByCurrency;
+	Double						maxBudgetAcceptedByCurrency;
+	Double						maxBudgetDeniedByCurrency;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+}
