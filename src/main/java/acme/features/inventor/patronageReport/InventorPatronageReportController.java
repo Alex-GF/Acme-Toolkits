eb31ca2f9f0d9cbed29f583ca0cc014ec160aaba
marreylop4 <marreylop4@alum.us.es>
2022-04-10 12:06:17 +0200

    Tarea-065:HECHA

---
+++ src/main/java/acme/features/inventor/patronageReport/InventorPatronageReportController.java
@@ -0,0 +1,26 @@
+package acme.features.inventor.patronageReport;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.patronageReport.PatronageReport;
+import acme.framework.controllers.AbstractController;
+import acme.roles.Inventor;
+
+@Controller
+public class InventorPatronageReportController extends AbstractController<Inventor, PatronageReport> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected InventorPatronageReportListMineService	listMineService;
+	
+	// Constructors -----------------------------------------------------------
+
+	@PostConstruct
+	protected void initialise() {
+		super.addCommand("list", this.listMineService);
+	}
+}
