16290c945973ba10539f5c1f56b1cd0cd5ee1917
Jose Luis Garcia Marin <josgarmar31@alum.us.es>
2022-04-12 12:11:56 +0200

    Tarea 055: Hecha

---
+++ src/main/java/acme/features/authenticated/announcement/AuthenticatedAnnouncementRepository.java
@@ -0,0 +1,21 @@
+package acme.features.authenticated.announcement;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.announcement.Announcement;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AuthenticatedAnnouncementRepository extends AbstractRepository{
+
+	@Query("select a from Announcement a where a.creationMoment > :deadline")
+	Collection<Announcement> findRecentAnnouncements(Date deadline);
+
+	@Query("select a from Announcement a where a.id = :id")
+	Announcement getItemById(int id);
+	
+}
